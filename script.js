c4d6e14c3d0c25eebfc7bfe7dfd0be52701767b8
thesanjaysuthhar-crypto <thesanjaysuthhar@gmail.com>
2026-01-29 22:04:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("popupImg");
+const closeBtn = document.querySelector(".close-btn");
+document.querySelectorAll(".gallery-img").forEach(img => {
+    img.addEventListener("click", function() {
+        modal.style.display = "flex";
+        modalImg.src = this.src; // 
+        modalImg.style.animation = "zoom 0.3s ease"; 
+    });
+});
+closeBtn.onclick = () => {
+    modal.style.display = "none";
+};
+window.onclick = (e) => {
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
+};
